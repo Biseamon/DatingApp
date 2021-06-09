7112e2ecd87d03183c31640029ffb3aed7222c74
Marin Capranov <marin.capranov@bbconsult.co.uk>
2021-06-10 00:03:48 +0100

    Adding the likes functionality so called many to many relationship

--- API/Extensions/UserParams.cs
+++ API/Extensions/UserParams.cs
@@ -1,18 +1,10 @@
+using API.Helpers;
+
 namespace API.Extensions
 {
-    public class UserParams
+    public class UserParams : PaginationsParams
     {
-        private const int MaxPageSize = 50;
-
-        public int PageNumber { get; set; } = 1;
-
-        public int _pageSize = 10;
-
-        public int PageSize 
-        { 
-            get => _pageSize; 
-            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value ; 
-        }
+       
 
         public string CurrentUsername { get; set; }
         public string Gender { get; set; }
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -104,6 +104,16 @@ export class MembersService {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'like/' + username, {})
+  }
+
+  getLikes(predicate: string, pageNumber, pageSize) {
+    let params = this.getPagianationHeaders(pageNumber, pageSize);
+    params = params.append('predicate', predicate);
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'like', params);
+  }
+
   private getPaginatedResult<T>(url, params) {
     const paginationResult: PaginatedResult<T> = new PaginatedResult<T>();
     return this.http.get<T>(url, { observe: "response", params }).pipe(
